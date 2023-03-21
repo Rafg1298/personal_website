72bf759a3158bb9bc61e2831f7f24e75b35f20e4
Riley Golden <riley9460@gmail.com>
2023-03-20 22:26:13 -0700

    fixing potential issues

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
             </span> 
         </header>
         <div className='about'>
-          <img className='portrait' src='https://res.cloudinary.com/dxgndie5o/image/upload/c_limit,q_82,r_0,w_328/v1679037684/itsme-min_s38bgg.jpg' alt='A picture of me!'/> 
+          <img className='portrait' src='https://res.cloudinary.com/dxgndie5o/image/upload/c_limit,q_82,r_0,w_328/v1679037684/itsme-min_s38bgg.jpg' alt='me in hawaii!'/> 
           <h2 className='me'>About me</h2>
           <span className='text'>
             <p>I started learning coding as a whole through web development first, and transitioned to more data-centric development after I took a data science class</p>
@@ -35,12 +35,12 @@ function App() {
             </span>
         </div>
         <div className='social' >
-          <a className='linked' href='https://www.linkedin.com/in/rileygolden/' target='_blank'>
+          <a className='linked' href='https://www.linkedin.com/in/rileygolden/' target='_blank' rel='noreferrer'>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill='currentcolor' d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 
         32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 
         38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
             </a>&nbsp; 
-            <a className='git' href='#' target='_blank'>
+            <a className='git' href='https://github.com/Rafg1298/' target='_blank'>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill='currentcolor' d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 
             1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 
             8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 
@@ -59,7 +59,5 @@ function App() {
     </div>
   );
 }
-
-
 
 export default App;
