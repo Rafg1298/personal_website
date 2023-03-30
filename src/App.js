dd5c8bcef014ebbe7510bd6a310fa95617ec31aa
Riley Golden <63813714+Rafg1298@users.noreply.github.com>
2023-03-29 23:34:10 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import './BlogPage';
-{/* import Card from '@mui/material/Card';
+/* import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material'; 
- */}
+ */
 
 
 function show() {
